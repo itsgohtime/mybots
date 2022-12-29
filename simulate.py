55ec1322924d2487270a72dc36c4daca3e683a24
Joel <joelxjgoh@gmail.com>
2022-12-28 23:59:11 -0800

    "Floor and gravity"

--- simulate.py
+++ simulate.py
@@ -1,7 +1,11 @@
 import pybullet as p
+import pybullet_data
 import time
 
 physicsClient = p.connect(p.GUI)
+p.setAdditionalSearchPath(pybullet_data.getDataPath())
+p.setGravity(0,0,-9.8)
+planeID = p.loadURDF("plane.urdf")
 p.loadSDF("box.sdf")
 for x in range(1000):
     p.stepSimulation()
