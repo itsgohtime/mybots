95db0a80ad9c1664429cebd15e71df543c0fc6e7
Joel <joelxjgoh@gmail.com>
2022-12-29 00:54:35 -0800

    "Finished joints"

--- generate.py
+++ generate.py
@@ -1,18 +1,35 @@
 import pyrosim.pyrosim as pyrosim
 
-pyrosim.Start_SDF("box_towers.sdf")
-length = 1; width = 1; height = 1
-x = 0; y = 0; z = 0.5
-for row in range(5):
-    for col in range(5):
-        for i in range(10):
-            box_name = "Box" + str(x)
-            pyrosim.Send_Cube(name=box_name, pos = [x, y, z + i], size = [length, width, height])
-            length = 0.9 * length
-            width = 0.9 * width
-            height = 0.9 * height
-        length = 1; width = 1; height = 1
-        x += 1
-    x = 0
-    y += 1
-pyrosim.End()
+def Create_World():
+    pyrosim.Start_SDF("world.sdf")
+    pyrosim.Send_Cube(name="Box", pos = [-3,3,0.5], size = [1,1,1])
+    pyrosim.End()
+
+def Create_Robot1():
+    pyrosim.Start_URDF("body.urdf")
+    pyrosim.Send_Cube(name="Link0", pos = [0,0,0.5], size = [1,1,1])
+    pyrosim.Send_Joint(name = "Link0_Link1", parent= "Link0", child = "Link1", type = "revolute", position = [0,0,1])
+    pyrosim.Send_Cube(name="Link1", pos = [0,0,0.5], size = [1,1,1])
+    pyrosim.Send_Joint(name="Link1_Link2", parent= "Link1", child="Link2", type="revolute", position=[0,0,1])
+    pyrosim.Send_Cube(name="Link2", pos=[0,0,0.5], size = [1,1,1])
+    pyrosim.Send_Joint(name="Link2_Link3", parent= "Link2", child="Link3", type="revolute", position=[0.5,0,0.5])
+    pyrosim.Send_Cube(name="Link3", pos=[0.5,0,0], size = [1,1,1])
+    pyrosim.Send_Joint(name="Link3_Link4", parent= "Link3", child="Link4", type="revolute", position=[1,0,0])
+    pyrosim.Send_Cube(name="Link4", pos=[0.5,0,0], size = [1,1,1])
+    pyrosim.Send_Joint(name="Link4_Link5", parent= "Link4", child="Link5", type="revolute", position=[0.5,0,-0.5])
+    pyrosim.Send_Cube(name="Link5", pos=[0,0,-0.5], size = [1,1,1])
+    pyrosim.Send_Joint(name="Link5_Link6", parent= "Link5", child="Link6", type="revolute", position=[0,0,-1])
+    pyrosim.Send_Cube(name="Link6", pos=[0,0,-0.5], size = [1,1,1])
+    pyrosim.End()
+
+def Create_Robot2():
+    pyrosim.Start_URDF("body.urdf")
+    pyrosim.Send_Cube(name="Torso", pos = [1.5,0,1.5], size = [1,1,1])
+    pyrosim.Send_Joint(name = "Torso_FrontLeg", parent= "Torso", child = "FrontLeg", type = "revolute", position = [2,0,1])
+    pyrosim.Send_Cube(name="FrontLeg", pos = [0.5,0,-0.5], size = [1,1,1])
+    pyrosim.Send_Joint(name = "Torso_BackLeg", parent= "Torso", child = "BackLeg", type = "revolute", position = [1,0,1])
+    pyrosim.Send_Cube(name="BackLeg", pos = [-0.5,0,-0.5], size = [1,1,1])
+    pyrosim.End()
+
+Create_World()
+Create_Robot2()
