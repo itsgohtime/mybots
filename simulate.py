b0cca773dfa285eac15be081a66a32544e9d005d
Joel <joelxjgoh@gmail.com>
2022-12-29 01:03:06 -0800

    "Step 14"

--- simulate.py
+++ simulate.py
@@ -1,4 +1,5 @@
 import pybullet as p
+import pyrosim.pyrosim as pyrosim
 import pybullet_data
 import time
 
@@ -8,8 +9,10 @@ p.setGravity(0,0,-9.8)
 planeID = p.loadURDF("plane.urdf")
 robotID = p.loadURDF("body.urdf")
 p.loadSDF("world.sdf")
+pyrosim.Prepare_To_Simulate(robotID)
 for x in range(1000):
     p.stepSimulation()
+    backLegTouch = pyrosim.Get_Touch_Sensor_Value_For_Link("BackLeg")
+    print(backLegTouch)
     time.sleep(1/60)
-    print(x)
 p.disconnect()
