3cf924b63e63cdb0baa14aa0b70fe4f67a79a8c7
Joel <joelxjgoh@gmail.com>
2022-12-31 23:08:24 -0800

    "Finished sensors"

--- analyze.py
+++ analyze.py
@@ -1,7 +1,9 @@
 import numpy
 import matplotlib.pyplot
 
-backLegSensorValues = numpy.load("data/BackLegSensorValues.npy")
-print(backLegSensorValues)
-matplotlib.pyplot.plot(backLegSensorValues)
+backLegSensorValues = numpy.load("data/backLegSensorValues.npy")
+frontLegSensorValues = numpy.load("data/frontLegSensorValues.npy")
+matplotlib.pyplot.plot(backLegSensorValues, label = "back", linewidth = 3)
+matplotlib.pyplot.plot(frontLegSensorValues, label = 'front', linewidth = 0.75)
+matplotlib.pyplot.legend()
 matplotlib.pyplot.show()
